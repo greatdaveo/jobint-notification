3d96f914cca551a4feaec550c8d5a1939a2988d8
David Olowomeye <olowomeyedavid242@gmail.com>
2024-07-03 16:40:15 +0100

    Completed the notification service connection and started the notification service

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { StatusCodes } from 'http-status-codes';
 const router: Router = express.Router();
 
 export function healthRoutes(): Router {
-  router.get('/notification-health', (req: Request, res: Response) => {
+  router.get('/notification-health', (_req: Request, res: Response) => {
     res.status(StatusCodes.OK).send('Notification service is healthy and OK');
   });
 
